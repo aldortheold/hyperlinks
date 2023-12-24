bee48fef1d7501b416f3df1dfa43d80508e91e86
Andrew <145247800+aldortheold1@users.noreply.github.com>
2023-12-24 22:46:01 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function changeField(){
-    if (document.getElementById("crossword").src == "http://127.0.0.1:5500/img/crossword_empty.png"){
+    if (document.getElementById("crossword").src == "https://i.imgur.com/Prtqcnx.png"){
         document.getElementById("crossword").src = "img/crossword.png";
         document.getElementById("checkAnswers").innerHTML = "Скрыть ответы";
     }
     else{
-        document.getElementById("crossword").src = "http://127.0.0.1:5500/img/crossword_empty.png";
+        document.getElementById("crossword").src = "https://i.imgur.com/Prtqcnx.png";
         document.getElementById("checkAnswers").innerHTML = "Проверить ответы";
     }
 }
